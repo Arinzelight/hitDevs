d81a51f2c0bd1feb8cf3096dd8b1da4c9368ae58
Arinzelight <arinzelight2@gmail.com>
2023-05-20 07:44:25 +0100

    si

--- SI.js
+++ SI.js
@@ -3,9 +3,10 @@ const years = document.getElementById('years');
 const rate = document.getElementById('rate');
 
 const result = document.getElementById('result');
+const compute = document.querySelector('.btn');
 
 
-document.addEventListener('click', function(){
+compute.addEventListener('click', function(){
     P = principal.value;
     T = years.value;
     R = rate.value;
@@ -14,10 +15,10 @@ document.addEventListener('click', function(){
     if (P <= 0) {
         alert('Please enter a positive number!');
         document.getElementById("principal").focus();
-    }else if(T === 1) {
-        result.innerHTML = `Your Simple Interest on "<mark>"N${P} for ${T}year at ${R}% rate is N${interest}`
+    } else if(T == 1){
+        result.innerHTML = `<mark> Your Simple Interest on N${P} for ${T}year at ${R}% rate is N${interest}`
     }else {
-        result.innerHTML = `Your Simple Interest on "<mark>"N${P} for ${T}years at ${R}% rate is N${interest}`
+            result.innerHTML = `<mark> Your Simple Interest on N${P} for ${T}years at ${R}% rate is N${interest}`
     }
   
 });
